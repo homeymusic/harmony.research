28a2ab82adcfc65d9e49628179bae5f4cc6eed75
Brian Mulloy <brian@landlessness.net>
2022-09-15 21:50:24 -0400

    guessing reference tone if not provided

--- R/harmony.R
+++ R/harmony.R
@@ -1,7 +1,9 @@
 harmony.uncached <- function(x, direction=+1, reference_tone=NULL, name=NULL) {
   checkmate::assert_integerish(x)
   checkmate::assert_choice(direction,c(-1,+1))
-  checkmate::assert_integerish(reference_tone)
+  reference_tone = ifelse(is.null(reference_tone),
+                          guess_reference_tone(x,direction),
+                          reference_tone)
 
   t = tibble::tibble(
     position = x %>% mean,
@@ -32,18 +34,30 @@ harmony <- memoise::memoise(harmony.uncached)
 #' @export
 h <- harmony
 
+guess_reference_tone <- function(x,direction) {
+  ifelse(direction>0,min(x),max(x))
+}
+
 intervallic_name <- function(x, direction, reference_tone) {
   checkmate::assert_integerish(x)
   checkmate::assert_choice(direction,c(-1,+1))
   checkmate::assert_integerish(reference_tone)
 
-  underlined_reference_tone = stringr::str_replace_all(reference_tone,"(.)","\\1\u0332")
+  underline =  '\u0332' # 0̲ underlines the character preceding the unicode
+  up_arrow =   '\u21D1' # ⇑
+  down_arrow = '\u21D3' # ⇓
+
+  underlined_reference_tone = stringr::str_replace_all(reference_tone,
+                                                       "(.)",
+                                                       paste0("\\1",underline))
   intervallic_name = x %>% sort %>% paste(collapse = ":")
   if (reference_tone %in% x) {
-    intervallic_name=gsub(paste0("\\b",reference_tone,"\\b"),underlined_reference_tone,intervallic_name)
+    intervallic_name=gsub(paste0("\\b",reference_tone,"\\b"),
+                          underlined_reference_tone,intervallic_name)
   } else {
     intervallic_name = paste(underlined_reference_tone,intervallic_name)
   }
-  intervallic_name = paste(intervallic_name, ifelse (direction == 1, '\u21D1','\u21D3'), sep="")
+  intervallic_name = paste0(intervallic_name,
+                           ifelse (direction == 1, up_arrow,down_arrow))
   intervallic_name
 }
