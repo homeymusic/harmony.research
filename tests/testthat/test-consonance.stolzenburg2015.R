8beb582de39ff7f802fa3e7a38161b3d4650a42c
Brian Mulloy <brian@landlessness.net>
2022-10-10 17:44:12 -0400

    found a problem. added a test for it. do not know the solution yet.

--- tests/testthat/test-consonance.stolzenburg2015.R
+++ tests/testthat/test-consonance.stolzenburg2015.R
@@ -1,6 +1,11 @@
 test_that('modified relative periodicity is symmetrical',{
-  expect_equal(consonance.stolzenburg2015(c(0,4))$affinity,
-               consonance.stolzenburg2015(c(0,8))$affinity)
-  expect_equal(consonance.stolzenburg2015(c(0,4))$brightness,
-               -consonance.stolzenburg2015(c(0,8))$brightness)
+  M3_up = consonance.stolzenburg2015(c(0,4))
+  m6_up = consonance.stolzenburg2015(c(0,8))
+  expect_equal(M3_up$affinity,m6_up$affinity)
+  expect_equal(M3_up$brightness,-m6_up$brightness)
+
+  M3_down = consonance.stolzenburg2015(c(0,4),-1)
+  m6_down = consonance.stolzenburg2015(c(0,8),-1)
+  expect_equal(M3_down$affinity, m6_down$affinity)
+  expect_equal(M3_down$brightness,-m6_down$brightness)
 })
